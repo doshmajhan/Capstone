8003554e9078468dc668dfd9ef87a505ba0fe71e
Doshmajhan <cpc1007@g.rit.edu>
2018-03-07 14:59:50 -0500

    adding initial docker file for a test ubuntu container with ssh installed, added test ansbile config to install apache on our generated container. The terraform file base_conf.tf ties it all together, will start up the docker container from our generated image and then provision it with ansible

---
+++ base_conf.tf
@@ -0,0 +1,19 @@
+# Configure the Docker provider
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+# Create a container
+resource "docker_container" "foo" {
+  image = "ubuntu_ssh"
+  name  = "foo"
+  ports {
+    internal = 22
+    external = 2222
+  }
+
+  provisioner "local-exec" {
+      command = "sleep 10; ansible-playbook config.yml -i inventory.ini"
+  }
+}
+
