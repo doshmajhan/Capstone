71bb214318a0a48351e2218d7778a98ba7f3f274
Kyle Carretto <kcarretto@gmail.com>
2018-03-07 18:28:09 -0500

    Added API endpoint to dynamically add roles to the database

--- requirement_database/app/endpoint.py
+++ requirement_database/app/endpoint.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from mongoengine.errors import DoesNotExist
+from mongoengine.errors import DoesNotExist, NotUniqueError
 from .model import VulnerableRole
 API = Blueprint('endpoint', __name__)
 
@@ -16,3 +16,30 @@ def get_deps(role_name):
             'error': True,
             'description': 'Role does not exist'
         })
+
+@API.route('/add_role', methods=['POST'])
+def add_role():
+    data = request.get_json()
+    if data is None:
+        data = request.form
+    try:
+        role = VulnerableRole(
+            name=data['name'],
+            operating_systems=data['operating_systems'],
+            protected_ports=data['protected_ports'],
+            protected_files=data['protected_files']
+        )
+        role.save(force_insert=True)
+        return jsonify(
+            'error': False
+        )
+    except NotUniqueError:
+        return jsonify(
+            'error': True,
+            'description': 'Role already exists in the database',
+        )
+    except KeyError:
+        return jsonify(
+            'error': True,
+            'description': 'Missing required parameter'
+        )
