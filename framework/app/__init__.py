59d00d875080454c8747d12297649720cc5ee014
Kyle Carretto <kcarretto@gmail.com>
2018-04-15 17:57:10 -0400

    Fixed init import

--- framework/app/__init__.py
+++ framework/app/__init__.py
@@ -18,7 +18,7 @@ def create_app(**config_overrides):
     app = Flask(__name__)
     app.config.from_object('app.config')
     app.config['MONGODB_SETTINGS'] = {'db': 'requirements_db'}
-    from app.endpoint import API
+    from app.api import API
     app.register_blueprint(API)
     app.config.update(config_overrides)
     try:
